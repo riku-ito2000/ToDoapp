95193ed0a1ac1ce463e04488a43973a386c02172
riku-ito2000 <r_ito@craid-inc.com>
2024-05-01 16:18:04 +0900

    revised_edit

--- contents/a.php
+++ contents/a.php
@@ -6,7 +6,10 @@ $id = $_POST['id'];
 
 // データベースで対象の ToDo を更新
 require_once 'connect_db.php';
-connectDB();
+$pdo=connectDB();
 
 $stmt = $pdo->prepare("UPDATE todosTable SET title = ?, content = ? WHERE id = ?");
 $stmt->execute([$title, $content, $id]);
+
+header("Location: index.php");
+exit();
